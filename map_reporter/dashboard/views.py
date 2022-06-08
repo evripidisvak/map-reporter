0c44bc2e6f29578dc917fe3876046b73753361fd
evripidisvak <ev.vakalis@gmail.com>
2022-06-08 16:56:49 +0300

    Started product info page

--- map_reporter/dashboard/views.py
+++ map_reporter/dashboard/views.py
@@ -3,6 +3,7 @@ from itertools import product
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.views import View
+from django.views.generic.base import TemplateView
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, authenticate
 from reporter.models import *
@@ -36,9 +37,11 @@ class Login(View):
             return render(request, self.template, {'form': form})
 
 
-class ProductInfo(generic.DetailView):
-    model = Product
-    template_name = 'dashboard/product_info.html'
+# class ProductInfo(View):
+#     template = 'dashboard/product_info.html'
+
+#     def get_
+    
 
     # def get_queryset(self):
     #     return Product.objects.filter(pk = 2)
@@ -46,3 +49,25 @@ class ProductInfo(generic.DetailView):
 
     # def get(self, request):
     #     return render(request, self.template)
+
+# class ProductInfo(generic.DetailView):
+#     model = Product
+#     template_name = 'dashboard/product_info.html'
+
+#     def get_queryset(self):
+#         return Page.objects.filter(product_id = self.kwargs['pk'])
+    
+
+    # def get(self, request):
+    #     return render(request, self.template)
+
+class ProductInfo(TemplateView):
+    template_name = "dashboard/product_info.html"
+
+    def get_context_data(self, **kwargs):
+        context = context = super(ProductInfo, self).get_context_data(**kwargs)
+        context.update({
+            "product": Product.objects.get(id=kwargs['pk']),
+            "urls": Page.objects.filter(product_id=kwargs['pk'])
+        })
+        return context
