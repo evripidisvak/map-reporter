cbcb511f01f74b3f608daa00e903e6f4bf929d2b
Nick <nickzervos96@gmail.com>
2022-06-22 13:36:41 +0300

    commit-js

--- map_reporter/dashboard/templatetags/dashboard_tags.py
+++ map_reporter/dashboard/templatetags/dashboard_tags.py
@@ -1,5 +1,11 @@
 from django import template
 from django.contrib.auth.models import User
+from django.template.base import Node
+from django.utils.functional import keep_lazy as allow_lazy
+import six, os
+from mimetypes import guess_type
+from base64 import b64encode
+# from django.contrib.staticfiles import finders
 
 register = template.Library()
 
@@ -55,5 +61,43 @@ def per_add(value, arg):
         except Exception:
             return "-"
 
+@register.tag
+def linebreakless(parser, token):
+    nodelist = parser.parse(('endlinebreakless',))
+    parser.delete_first_token()
+    return LinebreaklessNode(nodelist)
+
+
+class LinebreaklessNode(Node):
+    def __init__(self, nodelist):
+        self.nodelist = nodelist
+
+    def render(self, context):
+        strip_line_breaks = allow_lazy(lambda x: x.replace('\n', ''), six.text_type)
+        return strip_line_breaks(self.nodelist.render(context).strip())
 
 # register.filter('sub', sub)
+
+
+@register.filter
+def datalize(filename, content_type=None):
+    """
+    This filter will return data URI for given file, for more info go to:
+    http://en.wikipedia.org/wiki/Data_URI_scheme
+    Sample Usage:
+    <img src="{{ 'image.jpg'|datalize }}"/> or 
+    <img src="{% static 'image.jpg'|datalize %}"/>
+    will be filtered into:
+    <img src="data:image/png;base64,iVBORw0...">
+    """
+    if filename:
+        #we do this because FUCKIN windows..
+        filename = filename.replace('/','',1).replace('/','\\')
+        with open(filename, "rb") as f:
+            data = f.read()
+
+        encoded = b64encode(data)
+        content_type, encoding = guess_type(filename)
+        return "data:%s;base64,%s" % (content_type, encoded.decode('utf-8'))
+    else:
+        return ''
